04a521906ea022354ab85bff01c205a4c2d773ea
MagicWinnie <magicwinnie@yandex.ru>
2020-06-19 17:03:10 +0700

    Fixed blue

--- ALL_IN_ONE.py
+++ ALL_IN_ONE.py
@@ -155,10 +155,11 @@ colors_p_hsv = {
 colors_p_hsv = {
     'green': (np.array([71, 86, 22]), np.array([88, 255, 255])),
     'yellow': (np.array([14, 75, 33]), np.array([37, 255, 255])),
-    'blue': (np.array([94, 95, 55]), np.array([132, 255, 255])),
+    'blue': (np.array([94, 88, 63]), np.array([134, 255, 255])),
     'red': (np.array([136, 78, 102]), np.array([174, 255, 255])),
     'brown': (np.array([0, 81, 91]), np.array([10, 255, 255])),
 }
+
 colors_p_rgb = {
     "yellow": [0,  200,  200],
     "red": [0, 0, 255],
