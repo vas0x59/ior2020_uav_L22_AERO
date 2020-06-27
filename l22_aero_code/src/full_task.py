608a91d0664cdd0be6f0645e4b6dc3bf8902d354
vas0x59 <vasily.yuryev.a1@gmail.com>
2020-06-27 23:10:47 +0700

    ALL_IN_ONE -> full_task.py

--- l22_aero_code/src/full_task.py
+++ l22_aero_code/src/full_task.py
@@ -27,7 +27,7 @@ import os
 from mavros_msgs.srv import CommandBool
 arming = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
 
-sys.path.append('/home/dmitrii/catkin_ws/src/ior2020_uav_L22_AERO')
+# sys.path.append('/home/dmitrii/catkin_ws/src/ior2020_uav_L22_AERO')
 sys.path.append('/home/pi/catkin_ws/src/ior2020_uav_L22_AERO')
 from l22_aero_vision.msg  import ColorMarker
 from l22_aero_vision.msg  import ColorMarkerArray
@@ -133,14 +133,19 @@ def land():
     Фунцкия для посадки
     '''
     land_serv()
-    rospy.sleep(5)
+    rospy.sleep(0.79)
     arming(False)
 
 class ColorMarkerMap:
-    def __init__(self, cx_map=0, cy_map=0, cz_map=0, cx_img=0, cy_img=0, color="none"):
+    def __init__(self, cx_map=0, cy_map=0, cz_map=0, cx_img=0, cy_img=0, color="none", cx_cam=0, cy_cam=0, cz_cam=0):
         self.cx_map = cx_map
         self.cy_map = cy_map
         self.cz_map = cz_map
+
+        self.cx_cam = cx_cam
+        self.cy_cam = cy_cam
+        self.cz_cam = cz_cam
+
         self.cx_img = cx_img
         self.cy_img = cy_img
         self.color = color
@@ -160,7 +165,7 @@ class Recognition:
         self.cv_image = np.zeros((240, 320, 3), dtype="uint8")
         self.image_sub = rospy.Subscriber('image_raw', Image, self.image_callback)
         self.qr_pub = rospy.Publisher('/qr_debug', Image, queue_size=1)
-        self.coords_sub = sub = rospy.Subscriber("/l22_aero_color/markers", ColorMarkerArray, self.markers_arr_clb)
+        self.coords_sub  = rospy.Subscriber("/l22_aero_color/markers", ColorMarkerArray, self.markers_arr_clb)
         self.circles_sub = rospy.Subscriber("/l22_aero_color/circles", ColorMarkerArray, self.circles_arr_clb)
         self.result = []
         self.circles = []
@@ -173,9 +178,13 @@ class Recognition:
         cx_map = 0
         cy_map = 0
         cz_map = 0
-        cx_map, cy_map, cz_map, _ = transform_xyz_yaw(
-            marker.cx_cam, marker.cy_cam, marker.cz_cam, 0, "main_camera_optical", frame_to, listener)
-        return ColorMarkerMap(color=marker.color, cx_map=cx_map, cy_map=cy_map, cz_map=cz_map)
+        try:
+            cx_map, cy_map, cz_map, _ = transform_xyz_yaw(
+                marker.cx_cam, marker.cy_cam, marker.cz_cam, 0, "main_camera_optical", frame_to, listener)
+        except (tf.LookupException, tf.ConnectivityException):
+            print("TF error")
+        return ColorMarkerMap(color=marker.color, cx_map=cx_map, cy_map=cy_map, cz_map=cz_map, 
+            cx_cam=marker.cx_cam, cy_cam=marker.cy_cam, cz_cam=marker.cz_cam)
 
     def markers_arr_clb(self, msg):
         '''
@@ -231,33 +240,35 @@ class Recognition:
                 color = self.result[i].color
             # if (self.result[i].cz_map - Z) < Z_TOL:
             tempCoords = (self.result[i].cx_map, self.result[i].cy_map)
-            if tempCoords[0] < 0 or tempCoords[1] < 0: continue
+            if tempCoords[0] < -1.5 or tempCoords[1] < -1.5: continue
             if len(coordinates[color]) == 0:
-                coordinates[color].append(tempCoords)
+                coordinates[color].append(np.array([np.array(tempCoords)]))
             else:
                 for j in range(len(coordinates[color])):
-                    if self.distance(coordinates[color][j], tempCoords) <= TOLERANCE_COORDS:
-                        coordinates[color][j] = self.average(tempCoords, coordinates[color][j])
+                    # print(coordinates[color][j].mean(axis=0), tempCoords, "arr", coordinates[color][j])
+                    if self.distance(coordinates[color][j].mean(axis=0), tempCoords) <= TOLERANCE_COORDS:
+                        coordinates[color][j] = np.append(coordinates[color][j], np.array([tempCoords]), axis=0)
                         break
                 else:
-                    coordinates[color].append(tempCoords)
+                    coordinates[color].append(np.array([np.array(tempCoords)]))
         self.result = []
         for i in range(len(self.circles)):
             if self.circles[i].color not in coordinates:
                 color = circle_type_mapping[self.circles[i].color]
             else:
                 color = self.circles[i].color
-            tempCoords = (self.circles[i].cx_map, self.circles[i].cy_map)
+            tempCoords = [self.circles[i].cx_map, self.circles[i].cy_map]#####################################
             if tempCoords[0] < -1 or tempCoords[1] < -1: continue #DELETE IF NEEDED!
             if len(coordinates[color]) == 0:
-                coordinates[color].append(tempCoords)
+                coordinates[color].append(np.array([np.array(tempCoords)]))##################################################
             else:
                 for j in range(len(coordinates[color])):
-                    if self.distance(coordinates[color][j], tempCoords) <= TOLERANCE_COORDS:
-                        coordinates[color][j] = self.average(tempCoords, coordinates[color][j])
+                    if self.distance(coordinates[color][j].mean(axis=0), tempCoords) <= TOLERANCE_COORDS:
+                        # coordinates[color][j] = list(self.average(tempCoords, coordinates[color][j])) + [coordinates[color][j][2] + 1] ################################
+                        coordinates[color][j] = np.append(coordinates[color][j], np.array([tempCoords]), axis=0)
                         break
                 else:
-                    coordinates[color].append(tempCoords)
+                    coordinates[color].append(np.array([np.array(tempCoords)]))######coordinates[color].append(list(tempCoords) + [1]) #########################################################
         self.circles = []
 
     def coords_thread_func(self):
@@ -288,14 +299,15 @@ rc = Recognition()
 
 
 z = 1.5
-FIELD_LENGTH = 3.23 #in meters
-deltaX = 0.5 #in meters
-deltaY = 0.4 #in meters
+FIELD_LENGTH_X = 2.83 #in meters
+FIELD_LENGTH_Y = 2.65 #in meters
+deltaX = 0.65 #in meters
+deltaY = 0.25 #in meters
 betweenX = 3
 LANDING_B = 5
 
 
-i, count = 0, 0
+i, count = 0.1, 0
 points = []
 
 def getAdditionalPoints(coord1, coord2, parts, xyz=0):
@@ -308,48 +320,51 @@ def getAdditionalPoints(coord1, coord2, parts, xyz=0):
 
 
 # Создание массива с точками для дальнейшего полета по полю (полет по зиг-загу)
-while i <= FIELD_LENGTH:
-    j = 0
-    while j <= FIELD_LENGTH:
+while i <= FIELD_LENGTH_X:
+    j = 0.1
+    while j <= FIELD_LENGTH_Y:
         if count % 2 == 0:
             points.append((i, j))
         else:
-            points.append((i, FIELD_LENGTH-j))
+            points.append((i, FIELD_LENGTH_Y-j))
         j += deltaY
-    d = j - FIELD_LENGTH
+    d = j - FIELD_LENGTH_Y
     if d > 0: j -= d
-    if count % 2 == 0:
+    if count % 2 == 0 and i != len(points) - 1:
         points += list(getAdditionalPoints((i, j), (i + deltaX, j), betweenX))
-    else:
-        points += list(getAdditionalPoints((i, FIELD_LENGTH - j), (i + deltaX, FIELD_LENGTH-j), betweenX))
+    elif count % 2 != 0 and i != len(points) - 1:
+        points += list(getAdditionalPoints((i, FIELD_LENGTH_Y - j), (i + deltaX, FIELD_LENGTH_Y-j), betweenX))
     i += deltaX
     count += 1
 
-if points[-1][0] > FIELD_LENGTH:
-    points = points[:-1]
+if points[-1][0] > FIELD_LENGTH_X:
+    points = points[:-3]
 
 # взлет
 takeoff(z)
-navigate_wait(0, 0, 1.2, yaw = 3.14/2)
+navigate_wait(0.15, 0.15, 1.2)
 
 # распознавание qr-кода
 qrs = []
 qr = 'seed'
-zLower = 1.2
+zLower = 1.05
 
 # полет вокруг qr-кода для улучшения распознавания
-for (x_new, y_new) in [(0, 0), (0.15, 0), (0.2, 0), (0.2, 0.15), (0.2, 0.2), (0, 0)]:
+for (x_new, y_new) in [(0.12, 0.15), (0.18, 0.05), (0.20, 0.05), (0.23, 0.2), (0.2, 0.25), (0.15, 0.15)]:
     navigate_wait(x_new, y_new, zLower)
     qrs.append(rc.waitDataQR())
+    rospy.sleep(0.55)
 
 if len(qrs) > 0:
     qr = rc.most_frequent(qrs)
 
-if qr == None: qr = 'seed'
+if qr == None: 
+    qr = 'seed'
+    print(".....")
 
 print(qr)
 
-navigate_wait(0, 0, z)
+navigate_wait(0.15, 0.1, z)
 
 # полет по полю
 for point in points:
@@ -357,32 +372,93 @@ for point in points:
     if points.index(point) == int(len(points) // 4):
         break
     '''
-    navigate_wait(x=point[0], y=point[1], z=z)
+    navigate_wait(x=point[0], y=point[1], z=z, speed=0.4, yaw=3.14/2.0)
+    rospy.sleep(0.3)
     rc.coordsFunc()
+    rospy.sleep(0.3)
+print("739")
 
+# for (x_new, y_new) in [(0.75*FIELD_LENGTH_X, 0.75*FIELD_LENGTH_Y), (0.75*FIELD_LENGTH_X, FIELD_LENGTH_Y/4), (FIELD_LENGTH_X/4, FIELD_LENGTH_Y/4), (FIELD_LENGTH_X/4, 0.75*FIELD_LENGTH_Y)]:
+#     navigate_wait(x_new, y_new, 2.1)
+#     rospy.sleep(1)
 
+    
+print(coordinates[circle_type_mapping[qr]])
 # определение координат для посадки
 if len(coordinates[circle_type_mapping[qr]]) == 0:
     landCoordinate = (1, 1)
+    print("1, 1")
 else:
-    landCoordinate = coordinates[circle_type_mapping[qr]][0]
-
+    landCoordinate = max(coordinates[circle_type_mapping[qr]], key=len).mean(axis=0) ###############################################################
+    print("landCoordinate", landCoordinate)
+print("746")
 # посадка
+
+telem = get_telemetry_aruco()
+for (x_new, y_new) in list(getAdditionalPoints((telem.x, telem.y), landCoordinate, 3)):
+    navigate_wait(x_new, y_new, z)
+
 navigate_wait(landCoordinate[0], landCoordinate[1], z)
+print("749")
+
+print('WRITING CSV WITH COORDINATES')
+print(coordinates)
+
+# Создание csv файла с координатами
+
+if not os.path.exists(os.environ['HOME']+"/L22_AERO_LOG"):
+    os.mkdir(os.environ['HOME']+"/L22_AERO_LOG")
+
+import csv
+# from time import time
+
+with open(os.environ['HOME']+"/L22_AERO_LOG/" + 'L22_AERO_result.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(["Sector", "Type", "x (cm)", "y (cm)"])
+    arr = []
+    for key in coordinates:
+        if key in ['water_land', 'seed_land', 'pastures_land']: continue
+        for j in range(len(coordinates[key])):
+            point = coordinates[key][j].mean(axis=0)
+            x = point[0]
+            y = point[1]
+            if x < FIELD_LENGTH_X/2 and y < FIELD_LENGTH_Y/2:
+                arr.append(['C', key, x*100, y*100])
+            elif x < FIELD_LENGTH_X/2 and y >= FIELD_LENGTH_Y/2:
+                arr.append(['A', key, x*100, y*100])
+            elif x >= FIELD_LENGTH_X/2 and y < FIELD_LENGTH_Y/2:
+                arr.append(['D', key, x*100, y*100])
+            elif x >= FIELD_LENGTH_X/2 and y >= FIELD_LENGTH_Y/2:
+                arr.append(['B', key, x*100, y*100])
+    arr.sort(key = lambda x: x[0])
+    writer.writerows(arr)
+    writer.writerow(['','','',''])
+    writer.writerow(['','','',''])
+    writer.writerow(['','','',''])
+    writer.writerow(['','','',''])
+    writer.writerow(['TIME:', str(time.time()), 'TIME:', str(time.time())])
+print('CSV SAVED')
+
 
 telem = get_telemetry_aruco()
 
 last = None
 
-landingPath = list(getAdditionalPoints((landCoordinate[0], landCoordinate[1], z), (landCoordinate[0], landCoordinate[1], 1), betweenX, xyz = 1))
+Z_LAND = 0.85
+
+landingPath = list(getAdditionalPoints((landCoordinate[0], landCoordinate[1], z), (landCoordinate[0], landCoordinate[1], Z_LAND), 3, xyz = 1))
+print(landingPath)
 j = 0
+print("756")
 markerType = circle_type_mapping[qr]
 while j < len(landingPath):
+    print(i, j)
+    print(rc.circles)
     circles_copy = list(rc.circles)
     if len(circles_copy) > 0:
         for i in range(len(circles_copy)):
             if rc.distance((circles_copy[i].cx_map, circles_copy[i].cy_map), landCoordinate) <= 0.6 and circle_type_mapping[circles_copy[i].color] == markerType:
-                navigate_wait(circles_copy[i].cx_map, circles_copy[i].cy_map, landingPath[j][2])
+                navigate_wait(circles_copy[i].cx_map, circles_copy[i].cy_map, landingPath[j][2], tolerance=0.15)
                 last = list(circles_copy)
                 break
         else:
@@ -396,48 +472,44 @@ while j < len(landingPath):
             navigate_wait(circles_copy[-1].cx_map, circles_copy[-1].cy_map, 1.5)
 
     telem = get_telemetry_aruco()
+print("777")
+# LANDING SYSTEM
+print("markerType_LANDING", markerType)
+print("STAGE2")
+time_st = time.time()
+TIMEOUT_H = 2.85
+landing_update_rate = rospy.Rate(3.7)
+# OFFSET = [2, 2] # pixels
 
-'''
-if z > 1:
-    for (x_new, y_new, z_new) in list(getAdditionalPoints((landCoordinate[0], landCoordinate[1], z), (landCoordinate[0], landCoordinate[1], 1), LANDING_B, xyz = 1)):
-        navigate_wait(x_new, y_new, z_new)
-'''
+
+while (time.time() - time_st) < TIMEOUT_H:
+    markers = [i for i in rc.circles if circle_type_mapping[i.color] == markerType]
+    print(markers)
+    if len(markers) > 0:
+        marker = markers[0]
+        x_b, y_b, z_b, _ = transform_xyz_yaw(
+                marker.cx_cam, marker.cy_cam, marker.cz_cam, 0, "main_camera_optical", "body", listener)
+        # nav_broadcaster.sendTransform(
+        #     (x_b, y_b, z_b),
+        #     tf.transformations.quaternion_from_euler(0, 0, 0),
+        #     rospy.Time.now(),
+        #     "landing_target",
+        #     "body"
+        # )
+        rospy.sleep(0.087)
+        print(x_b, y_b, z_b)
+        
+        set_position(x=x_b, y=y_b, z=-0.076, frame_id="body")
+        if abs(z_b) < 0.2:
+            break
+    landing_update_rate.sleep()
+
+
+print("LANDDDDDDDDDDDDDDDD")
 land()
-
-rospy.sleep(4)
+vr.stop()
+rospy.sleep(1.5)
 print("DISARM")
 arming(False)
-
-print('WRITING CSV WITH COORDINATES. PLEASE WAIT...')
-print(coordinates)
-
-# Создание csv файла с координатами
-
-if not os.path.exists(os.environ['HOME']+"/L22_AERO_LOG"):
-    os.mkdir(os.environ['HOME']+"/L22_AERO_LOG")
-
-import csv
-from time import time
-
-with open(os.environ['HOME']+"/L22_AERO_LOG/" + 'result_'+str(time())+'.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerow(["Sector", "Type", "x", "y"])
-    arr = []
-    for key in coordinates:
-        if key in ['water_land', 'seed_land', 'pastures_land']: continue
-        for j in range(len(coordinates[key])):
-            x = coordinates[key][j][0]
-            y = coordinates[key][j][1]
-            if x < FIELD_LENGTH/2 and y < FIELD_LENGTH/2:
-                arr.append(['C', key, x, y])
-            elif x < FIELD_LENGTH/2 and y >= FIELD_LENGTH/2:
-                arr.append(['A', key, x, y])
-            elif x >= FIELD_LENGTH/2 and y < FIELD_LENGTH/2:
-                arr.append(['D', key, x, y])
-            elif x >= FIELD_LENGTH/2 and y >= FIELD_LENGTH/2:
-                arr.append(['B', key, x, y])
-    arr.sort(key = lambda x: x[0])
-    writer.writerows(arr)
-
 print('DONE')
 
