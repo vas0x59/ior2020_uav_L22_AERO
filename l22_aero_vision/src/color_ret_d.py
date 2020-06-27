957e082a3931d0346231106e62eaa5789fbfba47
vas0x59 <vasily.yuryev.a1@gmail.com>
2020-06-27 20:56:46 +0700

    RENAME

--- l22_aero_vision/src/color_ret_d.py
+++ l22_aero_vision/src/color_ret_d.py
@@ -1,8 +1,5 @@
-#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# tested on python3
-# Иморт всего что надо
 import math
 import rospy
 import time
@@ -12,20 +9,26 @@ from std_msgs.msg import Int32, Header, Float32
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import numpy as np
-from l22_aero_vision.msg import ColorRectMarker, ColorRectMarkerArray
+from l22_aero_vision.msg import ColorMarker, ColorMarkerArray
+# import tf
+
+
 
 rospy.init_node('l22_aero_color_node', anonymous=True)
 bridge = CvBridge()
 
-markers_arr_pub = rospy.Publisher("/l22_aero_color/markers", ColorRectMarkerArray)
+markers_arr_pub = rospy.Publisher("/l22_aero_color/markers", ColorMarkerArray)
+image_pub = rospy.Publisher("/l22_aero_color/debug_img", Image)
+'''
+colors_p_hsv = {
+    "yellow": (np.array([8,  60,  60]), np.array([35,  255, 255])),
+    "red": (np.array([160, 80,  80]), np.array([255, 255, 255]), np.array([0, 80,  80]), np.array([8, 255, 255])),
+    "blue": (np.array([161,  56,  109]), np.array([181, 126, 151])),
+    "green": (np.array([90,  70,  70]), np.array([160, 255, 255])),
+    "brown": (np.array([160, 80,  80]), np.array([255, 255, 255]), np.array([0, 80,  80]), np.array([8, 255, 255]))
+}
+'''
 
-# colors_p_hsv = {
-#     "yellow": (np.array([8,  60,  60]), np.array([35,  255, 255])),
-#     "red": (np.array([160, 80,  80]), np.array([255, 255, 255]), np.array([0, 80,  80]), np.array([8, 255, 255])),
-#     "blue": (np.array([161,  56,  109]), np.array([181, 126, 151])),
-#     "green": (np.array([90,  70,  70]), np.array([160, 255, 255])),
-#     "brown": (np.array([160, 80,  80]), np.array([255, 255, 255]), np.array([0, 80,  80]), np.array([8, 255, 255]))
-# }
 colors_p_hsv = {
     'blue': (np.array([72, 121, 67]), np.array([180, 255, 255])),
     'green': (np.array([43, 121, 67]), np.array([116, 255, 255])),
@@ -38,23 +41,32 @@ colors_p_rgb = {
     "red": [0, 0, 255],
     "blue": [255, 0, 0],
     "green": [0, 255, 0],
-    "brown": [165, 42, 42]
+    "brown": [42, 42, 165]
 }
 
-MARKER_SIDE1_SIZE = 0.06 # in m
-MARKER_SIDE2_SIZE = 0.09 # in m
+type_mapping = {
+    'blue': 'N2_water',
+    'green': 'N2_pastures',
+    'yellow': 'N2_seed',
+    'red': 'N2_potato',
+    'brown': 'N2_soil'
+}
+
+MARKER_SIDE1_SIZE = 0.3 # in m
+MARKER_SIDE2_SIZE = 0.3 # in m
 OBJ_S_THRESH = 150
+OFFSET = 10 # pixels
 
 objectPoint = np.array([(-MARKER_SIDE1_SIZE / 2, -MARKER_SIDE2_SIZE / 2, 0), (MARKER_SIDE1_SIZE / 2, -MARKER_SIDE2_SIZE / 2, 0), 
                         (MARKER_SIDE1_SIZE / 2, MARKER_SIDE2_SIZE / 2, 0), (-MARKER_SIDE1_SIZE / 2, MARKER_SIDE2_SIZE / 2, 0)])
 # print("objectPoint shape:", objectPoint.shape)
-class ColorRect:
+class Color:
     def __init__(self, cx_img=0, cy_img=0, color="none", points_img=[]):
         self.cx_img = cx_img
         self.cy_img = cy_img
         self.color = color
         self.points_img = points_img
-class ColorRectMarker_p:
+class ColorMarker_p:
     def __init__(self, cx_img=0, cy_img=0, color="none", points_img=[], cx_cam=0, cy_cam=0, cz_cam=0):
         self.cx_img = cx_img
         self.cy_img = cy_img
@@ -63,21 +75,21 @@ class ColorRectMarker_p:
         self.cx_cam = cx_cam
         self.cy_cam = cy_cam
         self.cz_cam = cz_cam
-    def fromColorRect(self, cr):
+    def fromColor(self, cr):
         self.cx_img = cr.cx_img
         self.cy_img = cr.cy_img
         self.color = cr.color
         self.points_img = cr.points_img
         return self
     def toMsg(self):
-        return ColorRectMarker(color=self.color, cx_img=self.cx_img, cy_img=self.cy_img, cx_cam=self.cx_cam, cy_cam=self.cy_cam, cz_cam=self.cz_cam)
+        return ColorMarker(color=self.color, cx_img=self.cx_img, cy_img=self.cy_img, cx_cam=self.cx_cam, cy_cam=self.cy_cam, cz_cam=self.cz_cam, size1=MARKER_SIDE1_SIZE, size2=MARKER_SIDE2_SIZE)
     def __str__(self):
         return "color: {}\n  coords: {} {} {}".format(self.color, str(self.cx_cam), str(self.cy_cam), str(self.cz_cam))
 
 cameraMatrix = np.zeros((3, 3), dtype="float64")
 distCoeffs = np.zeros((8, 1), dtype="float64")
 has_cam_info = False
-def camera_info_clb(msg: CameraInfo):
+def camera_info_clb(msg):
     global has_cam_info, cameraMatrix, distCoeffs 
     if not has_cam_info:
         has_cam_info = True
@@ -99,65 +111,84 @@ def get_color_objs(image, hsv, color_params):
     debug_out = cv2.bitwise_and(image, image, mask=mask)
     return cnts, debug_out
 
-def get_color_rects(cnts, color_name):
+def img_colision_check(pnts, offset, image_shape=(240, 320, 3)):
+    minx1 = pnts[:, 0].min() > offset
+    miny1 = pnts[:, 1].min() > offset
+    minx2 = image_shape[1] - pnts[:, 0].max() > offset
+    miny2 = image_shape[0] - pnts[:, 1].max() > offset
+    return minx1 and minx2 and miny1 and miny2
+
+def get_color_rects(cnts, color_name, image_shape=(240, 320, 3)):
     result = []
     for cnt in cnts:
-        approx = cv2.approxPolyDP(cnt, 0.1 * cv2.arcLength(cnt, True), True)
-        if len(approx) == 4:
+        approx = cv2.approxPolyDP(cnt, 0.02 * cv2.arcLength(cnt, True), True)
+        rect = cv2.minAreaRect(cnt)
+        # print(rect)
+        if len(approx) == 4 and abs(1 - rect[1][0] / (rect[1][1] + 1e-7)) < 0.2:
             points_img = np.array([np.array(p[0]) for p in approx]) # ?
-            M = cv2.moments(cnt)
-            cX = int((M["m10"] / (M["m00"] + 1e-7)))
-            cY = int((M["m01"] / (M["m00"] + 1e-7)))
-            result.append(ColorRect(color=color_name, cx_img=cX, cy_img=cY, points_img=points_img))
+            if img_colision_check(points_img, OFFSET,image_shape=image_shape):
+                M = cv2.moments(cnt)
+                cX = int((M["m10"] / (M["m00"] + 1e-7)))
+                cY = int((M["m01"] / (M["m00"] + 1e-7)))
+                result.append(Color(color=color_name, cx_img=cX, cy_img=cY, points_img=points_img))
     return result
 
-def draw_cnts_colors(image, cnts, color_name):
+def draw_cnts_colors(image, cnts, color_name, t = 1):
     for cnt in cnts:
         M = cv2.moments(cnt)
         cX = int((M["m10"] / (M["m00"] + 1e-7)))
         cY = int((M["m01"] / (M["m00"] + 1e-7)))
         cv2.drawContours(image, [cnt], -1, colors_p_rgb[color_name], 2)
-        cv2.putText(image, color_name, (cX, cY),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, colors_p_rgb[color_name], 2, cv2.LINE_AA)
+        
     return image
-def draw_color_rect(image, cr: ColorRect):
+
+def draw_color_rect(image, cr, t = 1):
     for i, p in enumerate(cr.points_img):
         cv2.circle(image, tuple(p), 5, ((i+1)*(255//4), (i+1)*(255//4), (i+1)*(255//4)), -1)
     cv2.circle(image, (cr.cx_img, cr.cy_img), 5, colors_p_rgb[cr.color], -1)
+    if t:
+        cv2.rectangle(image,(cr.cx_img,cr.cy_img-15),(cr.cx_img+75,cr.cy_img+5),(255,255,255),-1)
+        cv2.putText(image, type_mapping[cr.color], (cr.cx_img, cr.cy_img),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, colors_p_rgb[cr.color], 1, cv2.LINE_AA)
+    else:
+        cv2.putText(image, cr.color, (cr.cx_img, cr.cy_img),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, colors_p_rgb[cr.color], 2, cv2.LINE_AA)
     return image
 
-def get_rect_pose(rect, op, cM, dC) -> ColorRectMarker_p:
+def get_rect_pose(rect, op, cM, dC):
     # print("shapes", op.shape, rect.points_img.shape)
     retval, rvec, tvec = cv2.solvePnP(np.array(op, dtype="float64"), np.array(rect.points_img, dtype="float64"), cM, dC)
-    return ColorRectMarker_p(cx_cam=tvec[0][0], cy_cam=tvec[1][0], cz_cam=tvec[2][0]).fromColorRect(rect)
+    return ColorMarker_p(cx_cam=tvec[0][0], cy_cam=tvec[1][0], cz_cam=tvec[2][0]).fromColor(rect)
 
-def img_clb(msg: Image):
+def img_clb(msg):
     global has_cam_info, cameraMatrix, distCoeffs, markers_arr_pub
     image = bridge.imgmsg_to_cv2(msg, "bgr8")
+    # print(image.shape)
     # image[:, :, 2] = np.clip(image[:, :, 2]*0.7, 0, 255)
     # image[:, :, 1] = np.clip(image[:, :, 1]*1.2, 0, 255)
     # image[:, :, 0] = np.clip(image[:, :, 0]*1.5, 0, 255)
     out = image.copy()
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    result_in_img_frame = [] # ColorRect
-    for c_name in ["blue"]:
+    result_in_img_frame = [] # Color
+    for c_name in ["blue", "yellow", "green", "red", "brown"]:
         cnts, d_img = get_color_objs(image, hsv, colors_p_hsv[c_name])
         draw_cnts_colors(out, cnts, c_name)
         result_in_img_frame += get_color_rects(cnts, c_name)
     for i in result_in_img_frame:
         draw_color_rect(out, i)
-    cv2.imshow("out", out)
+    # cv2.imshow("out", out)
     result = []
     if has_cam_info:
         for r in result_in_img_frame:
             result.append(get_rect_pose(r, objectPoint, cameraMatrix, distCoeffs))
         if len(result) > 0:
             print("RES: \n " + "\n ".join(map(str, result)))
-    cv2.waitKey(1)
+    # cv2.waitKey(1)
 
-    markers_arr = ColorRectMarkerArray(header=Header(stamp=rospy.Time.now(), frame_id="color_marker_cam"), markers=[r.toMsg() for r in result])
+    markers_arr = ColorMarkerArray(header=Header(stamp=rospy.Time.now(), frame_id="color_marker_cam"), markers=[r.toMsg() for r in result])
     markers_arr_pub.publish(markers_arr)
+    image_pub.publish(bridge.cv2_to_imgmsg(out, "bgr8"))
 
 
 image_sub = rospy.Subscriber(
@@ -165,6 +196,5 @@ image_sub = rospy.Subscriber(
 
 camera_info_sub = rospy.Subscriber(
     "/main_camera/camera_info", CameraInfo, camera_info_clb)
-
 
 rospy.spin()
